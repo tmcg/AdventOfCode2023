edb5509d231c1376375a2e60b9c19705e8d9e1da
Tony McGee <tmcgee.au@gmail.com>
2023-12-23 01:27:10 +1000

    Day 10 part 2 completed

--- advent2023/day-10/src/main.rs
+++ advent2023/day-10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{ HashMap, HashSet };
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 enum Compass {
@@ -31,7 +31,7 @@ enum SegmentType {
 }
 
 impl SegmentType {
-    fn find_exit(&self, from: &Compass) -> Option<Compass> {
+    fn find_exit(&self, from: Compass) -> Option<Compass> {
         match (self, from) {
             (SegmentType::NorthWest, Compass::North) => Some(Compass::West),
             (SegmentType::NorthSouth, Compass::North) => Some(Compass::South),
@@ -52,17 +52,18 @@ impl SegmentType {
 
 #[derive(Debug)]
 struct MazeSegment {
-    seg_type: SegmentType
+    seg_type: SegmentType,
 }
 
 #[derive(Debug)]
 struct PipeMaze {
     pos: HashMap<(i32, i32), MazeSegment>,
     start: (i32, i32),
+    size: (i32, i32),
 }
 
 impl PipeMaze {
-    fn next_pos(xy: &(i32, i32), to: &Compass) -> (i32, i32) {
+    fn next_pos(xy: (i32, i32), to: Compass) -> (i32, i32) {
         match to {
             Compass::North => (xy.0, xy.1 - 1),
             Compass::South => (xy.0, xy.1 + 1),
@@ -71,11 +72,15 @@ impl PipeMaze {
         }
     }
 
-    fn get_segment_type(&self, xy: &(i32, i32)) -> SegmentType {
-        self.pos.get(xy).map_or(SegmentType::Ground, |x| x.seg_type)
+    fn valid_pos(&self, xy: (i32, i32)) -> bool {
+        !(xy.0 < 0 || xy.0 >= self.size.0 || xy.1 < 0 || xy.1 >= self.size.1)
     }
 
-    fn has_exit(&self, xy: &(i32, i32), from: &Compass) -> bool {
+    fn get_segment_type(&self, xy: (i32, i32)) -> SegmentType {
+        self.pos.get(&xy).map_or(SegmentType::Ground, |x| x.seg_type)
+    }
+
+    fn has_exit(&self, xy: (i32, i32), from: Compass) -> bool {
         self.get_segment_type(xy).find_exit(from).is_some()
     }
 
@@ -89,36 +94,148 @@ impl PipeMaze {
         Compass::West,
         ]
         .into_iter()
-        .find(|c| seg_type.find_exit(c).is_some())
+        .find(|c| seg_type.find_exit(*c).is_some())
     }
 
     fn find_farthest(&self) -> i32 {
+        (self.find_main_loop().len() / 2) as i32
+    }
+
+    fn find_main_loop(&self) -> HashSet<(i32, i32)> {
+        let mut result: HashSet<(i32, i32)> = HashSet::new();
         let mut dir = self.first_exit().unwrap();
         let mut pos = (self.start.0, self.start.1);
-        let mut ct: i32 = 0;
 
         loop {
-            let next_pos = PipeMaze::next_pos(&pos, &dir);
-            let next_pipe = self.get_segment_type(&next_pos);
-            let next_dir = next_pipe.find_exit(&dir.reverse()).unwrap();
+            result.insert(pos);
+
+            pos = PipeMaze::next_pos(pos, dir);
+            dir = self
+                .get_segment_type(pos)
+                .find_exit(dir.reverse())
+                .unwrap();
             
-            //println!("pos={pos:?}, dir={dir:?}");
-            //println!("next_pos={next_pos:?}");
-            //println!("next_pipe={next_pipe:?}");
-            //println!("next_dir={next_dir:?}");
-            //println!("===");
-
-            pos = next_pos;
-            //pos.0 = next_pos.0;
-            //pos.1 = next_pos.1;
-            dir = next_dir;
-            ct += 1;
-
-            if next_pos.0 == self.start.0 
-               && next_pos.1 == self.start.1 {
-                return ct / 2;
+            if pos.0 == self.start.0
+               && pos.1 == self.start.1 {
+                return result;
             }
         }
+    }
+
+    fn find_nest(&self) -> i32 {
+        let mut hs_left: HashSet<(i32, i32)> = HashSet::new();
+        let mut hs_right: HashSet<(i32, i32)> = HashSet::new();
+        
+        let hs_loop = self.find_main_loop();
+        let st = hs_loop.iter()
+            .find(|x| self.get_segment_type(**x) == SegmentType::NorthSouth)
+            .expect("Unable to find north/south segment");
+
+        let mut dir = Compass::North;
+        let mut pos = (st.0, st.1);
+        let mut typ;
+
+        loop {
+            let old_dir = dir;
+
+            pos = PipeMaze::next_pos(pos, dir);
+            typ = self.get_segment_type(pos);
+            dir = typ.find_exit(dir.reverse()).unwrap();
+
+            let n = PipeMaze::next_pos(pos, Compass::North);
+            let s = PipeMaze::next_pos(pos, Compass::South);
+            let e = PipeMaze::next_pos(pos, Compass::East);
+            let w = PipeMaze::next_pos(pos, Compass::West);
+
+            //println!("{old_dir:?} {dir:?} {pos:?}");
+
+            match (old_dir, dir) {
+                (Compass::North, Compass::North) => {
+                    self.flood_fill(w, &hs_loop, &mut hs_left);
+                    self.flood_fill(e, &hs_loop, &mut hs_right);
+                }
+                (Compass::North, Compass::East) => {
+                    self.flood_fill(n, &hs_loop, &mut hs_left);
+                    self.flood_fill(w, &hs_loop, &mut hs_left);
+                }
+                (Compass::North, Compass::West) => {
+                    self.flood_fill(n, &hs_loop, &mut hs_right);
+                    self.flood_fill(e, &hs_loop, &mut hs_right);
+                }
+                (Compass::South, Compass::South) => {
+                    self.flood_fill(e, &hs_loop, &mut hs_left);
+                    self.flood_fill(w, &hs_loop, &mut hs_right);
+                }
+                (Compass::South, Compass::East) => {
+                    self.flood_fill(s, &hs_loop, &mut hs_right);
+                    self.flood_fill(w, &hs_loop, &mut hs_right);
+                }
+                (Compass::South, Compass::West) => {
+                    self.flood_fill(s, &hs_loop, &mut hs_left);
+                    self.flood_fill(e, &hs_loop, &mut hs_left);
+                }
+                (Compass::East, Compass::North) => {
+                    self.flood_fill(s, &hs_loop, &mut hs_right);
+                    self.flood_fill(e, &hs_loop, &mut hs_right);
+                }
+                (Compass::East, Compass::South) => {
+                    self.flood_fill(n, &hs_loop, &mut hs_left);
+                    self.flood_fill(e, &hs_loop, &mut hs_left);
+                }
+                (Compass::East, Compass::East) => {
+                    self.flood_fill(n, &hs_loop, &mut hs_left);
+                    self.flood_fill(s, &hs_loop, &mut hs_right);
+                }
+                (Compass::West, Compass::North) => {
+                    self.flood_fill(s, &hs_loop, &mut hs_left);
+                    self.flood_fill(w, &hs_loop, &mut hs_left);
+                }
+                (Compass::West, Compass::South) => {
+                    self.flood_fill(n, &hs_loop, &mut hs_right);
+                    self.flood_fill(w, &hs_loop, &mut hs_right);
+                }
+                (Compass::West, Compass::West) => {
+                    self.flood_fill(s, &hs_loop, &mut hs_left);
+                    self.flood_fill(n, &hs_loop, &mut hs_right);
+                }
+                _ => panic!("Invalid change of direction"),
+            }
+            
+            if pos.0 == st.0 && pos.1 == st.1 {
+                break;
+            }
+        }        
+
+        let left_len = hs_left.len() as i32;
+        let right_len = hs_right.len() as i32;
+        //println!("left len={:?}", left_len);
+        //println!("right len={:?}", right_len);
+
+        // Assume the smallest filled area is the nest.
+        // Doesn't work for the general case, but does
+        // for the subset of puzzle inputs provided.
+        if left_len < right_len { left_len } else { right_len }
+    }
+
+    fn flood_fill(&self,
+        xy: (i32, i32),
+        hs_loop: &HashSet<(i32, i32)>,
+        hs_fill: &mut HashSet<(i32, i32)>) {
+
+        if !self.valid_pos(xy) { return; }
+        if hs_fill.contains(&xy) { return; }
+        if hs_loop.contains(&xy) { return; }
+
+        hs_fill.insert(xy);
+
+        let n = PipeMaze::next_pos(xy, Compass::North);
+        self.flood_fill(n, hs_loop, hs_fill);
+        let s = PipeMaze::next_pos(xy, Compass::South);
+        self.flood_fill(s, hs_loop, hs_fill);
+        let e = PipeMaze::next_pos(xy, Compass::East);
+        self.flood_fill(e, hs_loop, hs_fill);
+        let w = PipeMaze::next_pos(xy, Compass::West);
+        self.flood_fill(w, hs_loop, hs_fill);
     }
 }
 
@@ -126,7 +243,8 @@ impl From<&str> for PipeMaze {
     fn from(item: &str) -> Self {
         let mut maze = PipeMaze {
             pos: HashMap::new(),
-            start: (0, 0)
+            start: (0, 0),
+            size: (0, 0),
         };
 
         item.split("\r\n")
@@ -156,15 +274,15 @@ impl From<&str> for PipeMaze {
         });
 
         // Find the real segment type of the start position
-        let n0 = PipeMaze::next_pos(&maze.start, &Compass::North);
-        let s0 = PipeMaze::next_pos(&maze.start, &Compass::South);
-        let e0 = PipeMaze::next_pos(&maze.start, &Compass::East);
-        let w0 = PipeMaze::next_pos(&maze.start, &Compass::West);
+        let n0 = PipeMaze::next_pos(maze.start, Compass::North);
+        let s0 = PipeMaze::next_pos(maze.start, Compass::South);
+        let e0 = PipeMaze::next_pos(maze.start, Compass::East);
+        let w0 = PipeMaze::next_pos(maze.start, Compass::West);
 
-        let n1 = maze.has_exit(&n0, &Compass::South);
-        let s1 = maze.has_exit(&s0, &Compass::North);
-        let e1 = maze.has_exit(&e0, &Compass::West);
-        let w1 = maze.has_exit(&w0, &Compass::East);
+        let n1 = maze.has_exit(n0, Compass::South);
+        let s1 = maze.has_exit(s0, Compass::North);
+        let e1 = maze.has_exit(e0, Compass::West);
+        let w1 = maze.has_exit(w0, Compass::East);
 
         let start_type = match (n1,s1,e1,w1) {
             (true,true,false,false) => SegmentType::NorthSouth,
@@ -179,6 +297,11 @@ impl From<&str> for PipeMaze {
         if let Some(start) = maze.pos.get_mut(&maze.start) {
             start.seg_type = start_type;
         }
+
+        maze.size = (
+            maze.pos.keys().map(|k| k.0 + 1).max().unwrap_or(0),
+            maze.pos.keys().map(|k| k.1 + 1).max().unwrap_or(0),
+        );
         
         maze
     }
@@ -191,9 +314,9 @@ pub fn part1() -> String {
 }
 
 pub fn part2() -> String {
-    //let input = include_str!("../input1.txt");
-    //let maze = PipeMaze::from(input);
-    "".to_string()
+    let input = include_str!("../input1.txt");
+    let maze = PipeMaze::from(input);
+    maze.find_nest().to_string()
 }
 
 fn main() {
@@ -207,10 +330,10 @@ mod tests {
 
     #[test]
     fn next_pos() {
-        assert_eq!(PipeMaze::next_pos(&(3, 4), &Compass::North), (3, 3));
-        assert_eq!(PipeMaze::next_pos(&(3, 4), &Compass::South), (3, 5));
-        assert_eq!(PipeMaze::next_pos(&(3, 4), &Compass::East), (4, 4));
-        assert_eq!(PipeMaze::next_pos(&(3, 4), &Compass::West), (2, 4));
+        assert_eq!(PipeMaze::next_pos((3, 4), Compass::North), (3, 3));
+        assert_eq!(PipeMaze::next_pos((3, 4), Compass::South), (3, 5));
+        assert_eq!(PipeMaze::next_pos((3, 4), Compass::East), (4, 4));
+        assert_eq!(PipeMaze::next_pos((3, 4), Compass::West), (2, 4));
     }
 
     #[test]
@@ -219,15 +342,15 @@ mod tests {
         let ew = SegmentType::EastWest;
         let se = SegmentType::SouthEast;
 
-        assert_eq!(ns.find_exit(&Compass::North), Some(Compass::South));
-        assert_eq!(ew.find_exit(&Compass::East), Some(Compass::West));
-        assert_eq!(se.find_exit(&Compass::South), Some(Compass::East));
-        assert_eq!(se.find_exit(&Compass::East), Some(Compass::South));
-        assert_eq!(ns.find_exit(&ns.find_exit(&Compass::North).unwrap()), Some(Compass::North));
-        assert_eq!(ew.find_exit(&ew.find_exit(&Compass::East).unwrap()), Some(Compass::East));
-        assert_eq!(se.find_exit(&se.find_exit(&Compass::East).unwrap()), Some(Compass::East));
-        assert_eq!(se.find_exit(&Compass::North), None);
-        assert_eq!(se.find_exit(&Compass::West), None);
+        assert_eq!(ns.find_exit(Compass::North), Some(Compass::South));
+        assert_eq!(ew.find_exit(Compass::East), Some(Compass::West));
+        assert_eq!(se.find_exit(Compass::South), Some(Compass::East));
+        assert_eq!(se.find_exit(Compass::East), Some(Compass::South));
+        assert_eq!(ns.find_exit(ns.find_exit(Compass::North).unwrap()), Some(Compass::North));
+        assert_eq!(ew.find_exit(ew.find_exit(Compass::East).unwrap()), Some(Compass::East));
+        assert_eq!(se.find_exit(se.find_exit(Compass::East).unwrap()), Some(Compass::East));
+        assert_eq!(se.find_exit(Compass::North), None);
+        assert_eq!(se.find_exit(Compass::West), None);
     }
 
     #[test]
@@ -237,6 +360,8 @@ mod tests {
         
         assert_eq!(maze.pos.keys().len(), 25);
         assert_eq!(maze.start, (1, 1));
+        assert_eq!(maze.size, (5, 5));
+        assert!(maze.valid_pos((0, 0)));
 
         assert_eq!(maze.pos.get(&maze.start).unwrap().seg_type, SegmentType::SouthEast);
         assert_eq!(maze.pos.get(&(1,2)).unwrap().seg_type, SegmentType::NorthSouth);
@@ -246,7 +371,23 @@ mod tests {
     }
 
     #[test]
-    fn solve_sample() {
+    fn find_main_loop() {
+        let input2 = include_str!("../input2.txt");
+        let maze2 = PipeMaze::from(input2);
+        let hs = maze2.find_main_loop();
+        assert_eq!(hs.len(), 8);
+        assert!(hs.contains(&(1,1)));
+        assert!(hs.contains(&(1,2)));
+        assert!(hs.contains(&(1,3)));
+        assert!(hs.contains(&(2,1)));
+        assert!(hs.contains(&(2,3)));
+        assert!(hs.contains(&(3,1)));
+        assert!(hs.contains(&(3,2)));
+        assert!(hs.contains(&(3,3)));
+    }
+
+    #[test]
+    fn solve_sample_part1() {
         let input2 = include_str!("../input2.txt");
         let maze2 = PipeMaze::from(input2);
         assert_eq!(maze2.find_farthest(), 4);
@@ -257,12 +398,31 @@ mod tests {
     }
 
     #[test]
+    fn solve_sample_part2() {
+        let input2 = include_str!("../input2.txt");
+        let maze2 = PipeMaze::from(input2);
+        assert_eq!(maze2.find_nest(), 1);
+
+        let input4 = include_str!("../input4.txt");
+        let maze4 = PipeMaze::from(input4);
+        assert_eq!(maze4.find_nest(), 4);
+
+        let input5 = include_str!("../input5.txt");
+        let maze5 = PipeMaze::from(input5);
+        assert_eq!(maze5.find_nest(), 4);
+
+        let input6 = include_str!("../input6.txt");
+        let maze6 = PipeMaze::from(input6);
+        assert_eq!(maze6.find_nest(), 8);
+    }
+
+    #[test]
     fn solve_part1() {
         assert_eq!(part1(), "7030");
     }
 
     #[test]
     fn solve_part2() {
-        assert_eq!(part2(), "zz");
+        assert_eq!(part2(), "285");
     }
 }
